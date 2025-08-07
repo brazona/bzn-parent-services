c8c426598b9e6630918f820a8802abef12f34a0a
Cezar Silva <cezarfelipe2008@outlook.com>
2025-08-06 23:01:45 -0300

    feat: implementado estrutura inicial

---
+++ app/src/test/java/br/brazona/bzn_parent_services/BznParentServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.brazona.bzn_parent_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BznParentServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
